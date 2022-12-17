3cf8667663e2c72c1bfa53414fed920c323f2938
테스트 <test@gmail.com>
2022-12-17 11:31:26 +0900

    second

--- 1.py
+++ 1.py
@@ -1,2 +1,13 @@
-mon = 12222
-print(f'매출액 {mon:12}')
+# mon = 12222
+# print(f'매출액 {mon:12}')
+
+
+a = 'jeong yeon wook'
+
+len(a)   ##열 개수  띄어쓰기 포함
+print(len(a))
+print(a.count('y'))
+
+
+## .find(path) command 
+print(a.find('o'))
