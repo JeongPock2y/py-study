f5b6d1564ae51ef60bba9ab1743084142a39a4b3
테스트 <test@gmail.com>
2022-12-17 17:49:56 +0900

    d

--- createfile.py
+++ createfile.py
@@ -1,5 +1,17 @@
-#파일 객체 = open(파일 이름, 파일 열기 모드)
+# #파일 객체 = open(파일 이름, 파일 열기 모드)
 
-# f = open("C:/doit/py-study/py-study/newfile.py", 'w')
-with open("C:/doit/py-study/py-study/newfile1.txt", "w") as f:
-    f.write("Life is too short, you need python")
+# # f = open("C:/doit/py-study/py-study/newfile.py", 'w')
+# with open("C:/doit/py-study/py-study/newfile1.txt", "w") as f:
+#     f.write("Life is too short, you need python")
+import time
+
+print(time.asctime(time.localtime(time.time())))
+print(time.ctime())
+# class MyError(Exception):
+#     pass
+
+
+# def say_nick(nick):
+#     if nick == '바보':
+#         raise MyError()
+#     print(nick)
