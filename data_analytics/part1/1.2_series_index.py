005ba686bf08e832f5c25e0b0c21c8b67b8dd015
테스트 <test@gmail.com>
2022-12-18 15:51:31 +0900

    dataframe

--- data_analytics/part1/1.2_series_index.py
+++ data_analytics/part1/1.2_series_index.py
@@ -3,7 +3,7 @@
 import pandas as pd
 
 # 리스트를 시리즈로 변환하여 변수 sr에 저장
-list_data = ['2019-01-02', 3.14, 'ABC', 100, True]
+list_data = ['2022-12-18', 3.14, 'ABC', 100, True]
 sr = pd.Series(list_data)
 print(sr)
 print('\n')
