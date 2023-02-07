0a2bbf81c85079b7b7e225b0d43c10f5d63fa57e
JeongPock2y <rosopop@naver.com>
2023-02-07 14:39:35 +0900

    commit

---
+++ JupyterNB/random.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import random
+
+#데이터 입력
+fnameList = ["froyo.gif", "gingerbread.gif", "honeycomb.gif", "icecream.gif", "jellybean.gif", "kitkat.gif", "lollipop.gif", "marshmallow.gif", "nougat.gif"]
+random.shuffle(fnameList)
+print(fnameList)
